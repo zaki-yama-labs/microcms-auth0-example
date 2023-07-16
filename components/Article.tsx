fb336b156cd87ac97c18ebdc38e3de709e71089f
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2023-07-16 22:20:19 +0900

    2. ページ作成編

---
+++ components/Article.tsx
@@ -0,0 +1,28 @@
+import type { FC } from "react";
+import Image from "next/image";
+import type { ArticleListDetail } from "../types";
+
+type Props = {
+  data: ArticleListDetail;
+};
+
+const Article: FC<Props> = ({ data }) => {
+  const { thumbnail, title, body, publishedAt } = data;
+  return (
+    <main>
+      {thumbnail !== undefined && (
+        <Image
+          src={thumbnail.url}
+          width={thumbnail.width}
+          height={thumbnail.height}
+          alt=""
+        />
+      )}
+      <h1>{title}</h1>
+      <p>{publishedAt}</p>
+      <div dangerouslySetInnerHTML={{ __html: body || "" }} />
+    </main>
+  );
+};
+
+export default Article;
